efa2dcbe8a163ff691e126fa40e18937cb8da732
Konstantin Usichenko <usichenkokonstantinvladimirov@gmail.com>
2026-09-10 09:46:44 +0300

    Исправление по замечаниям

--- src/main/java/CheckGrade.java
+++ src/main/java/CheckGrade.java
@@ -3,15 +3,15 @@ public class CheckGrade {
     public static String getGrade(int score) {
 
         if (score >= 0 && score <= 20) {
-            return "0-20: E";
+            return "E";
         } else if (score >= 21 && score <= 40) {
-            return "21-40: D";
+            return "D";
         } else if (score >= 41 && score <= 60) {
-            return "41-60: C";
+            return "C";
         } else if (score >= 61 && score <= 80) {
-            return "61-80: B";
+            return "B";
         } else if (score >= 81 && score <= 100) {
-            return "81-100: A";
+            return "A";
         } else {
             return "Error";
         }
